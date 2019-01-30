9aeffde4e972841454769f34739a3351e1d6d39d
Dev-Bogdan <lisanets.b@place-start.ru>
2019-01-30 09:27:32 +0300

    добавлено хранение записей в БД

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 #!flask/bin/python
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
 from hashlib import md5
 
 
 app = Flask(__name__, static_url_path = "")
 auth = HTTPBasicAuth()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+
+class Note(db.Model):
+    '''Модель записи. Связана с БД'''
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1000), nullable=False)
+
+    def __repr__(self):
+        return '<Note %r>' % self.text
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return self
+
+    def make_public_note(self):
+        return {"text":self.text, "url":url_for('get_note', note_id=self.id, _external=True)}
 
 users = {
     "test": "23eeeb4347bdd26bfc6b7ee9a3b755dd",
@@ -17,7 +41,6 @@ def md5_verify(username, password):
         if users[username] == md5(str(password).encode('utf-8')).hexdigest():
             return True
     return False
-
 
 @auth.verify_password
 def verify_pw(username, password):
@@ -35,33 +58,18 @@ def not_found(error):
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-notes = [
-    {
-        'id': 1,
-        'text': 'Buy groceries',
-    },
-    {
-        'id': 2,
-        'title': 'Learn Python',
-    }
-]
-    
-def make_public_note(note):
-    note = dict((k, v) for k, v in note.items())
-    note['url'] = url_for('get_note', note_id=note.pop('id'), _external=True)
-    return note
 
 @app.route('/notebook/api/notes', methods = ['GET'])
 @auth.login_required
 def get_notes():
-    return jsonify( { 'notes': list(map(make_public_note, notes)) } )
+    return jsonify( { 'notes': [i.make_public_note() for i in Note.query.all()] } )
 
 @app.route('/notebook/api/notes/<int:note_id>', methods = ['GET'])
 @auth.login_required
 def get_note(note_id):
-    note = list(filter(lambda t: t['id'] == note_id, notes))
+    note = Note.query.get(note_id)
     if note:
-        return jsonify( { 'note': make_public_note(note[0]) } )
+        return jsonify( { 'note': note.make_public_note() } )
     abort(404)
 
 @app.route('/notebook/api/notes', methods = ['POST'])
@@ -69,34 +77,32 @@ def get_note(note_id):
 def create_note():
     if not request.json or not 'text' in request.json:
         abort(400)
-    note = {
-        'id': notes[-1]['id'] + 1,
-        'text': request.json['text'],
-    }
-    notes.append(note)
-    return jsonify( { 'note': make_public_note(note) } ), 201
+    note = Note(text=request.json['text'])
+
+    note.save()
+    return jsonify( { 'note': note.make_public_note() } ), 201
 
 @app.route('/notebook/api/notes/<int:note_id>', methods = ['PUT'])
 @auth.login_required
 def update_note(note_id):
-    note = list(filter(lambda t: t['id'] == note_id, notes))
+    note = Note.query.get(note_id)
     if not note:
         abort(404)
-    if not request.json:
+    if not (request.json and 'text' in request.json):
         abort(400)
-    if 'text' in request.json and type(request.json['text']) != unicode:
-        abort(400)
-    note[0]['text'] = request.json.get('text', note[0]['text'])
-    return jsonify( { 'note': make_public_note(note[0]) } )
+    note.text = request.json.get('text', note.text)
+    note.save()
+    return jsonify( { 'note': note.make_public_note() } )
     
 @app.route('/notebook/api/notes/<int:note_id>', methods = ['DELETE'])
 @auth.login_required
 def delete_note(note_id):
-    note = list(filter(lambda t: t['id'] == note_id, notes))
-    if len(note) == 0:
+    note = Note.query.get(note_id)
+    if not note:
         abort(404)
-    notes.remove(note[0])
+    note.delete()
     return jsonify( { 'result': True } )
     
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug = True)
